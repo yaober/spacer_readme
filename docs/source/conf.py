30b2b82f9f130125e2b855d9625fa144158b34a5
yaober <jia.yao@utsouthwestern.edu>
2025-10-15 15:03:57 -0500

    update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,26 +1,42 @@
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information
+# -- Project information -----------------------------------------------------
 
 project = 'SPACER'
 copyright = '2025, Jia Yao and Tao Wang'
-author = 'Graziella'
+author = 'Jia Yao'
 
 release = '1.0'
 version = '1.0.0'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
 
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'nbsphinx', 
+    'sphinx_copybutton',
+    'sphinxcontrib.bibtex',
+    'nbsphinx',
 ]
 
+# Let Sphinx read Jupyter notebooks but not execute them
+nbsphinx_allow_errors = True
+nbsphinx_execute = 'never'
 
+# Ignore some notebook warnings
+nbsphinx_prolog = """
+.. raw:: html
+
+   <style>
+       .nbinput.nblast, .nboutput.nblast { margin-bottom: 1em; }
+   </style>
+"""
+
+# -- Intersphinx -------------------------------------------------------------
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
@@ -29,17 +45,15 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-# -- Options for HTML output
-
+# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_logo = "_static/spacer_logo.png"
-html_favicon = "_static/favicon.ico"  # optional
+html_favicon = "_static/favicon.ico"
 
 html_theme_options = {
-    'logo_only': True,         
-    'display_version': False,  
+    'logo_only': True,
+    'display_version': False,
 }
 
-
-# -- Options for EPUB output
+# -- Options for EPUB output -------------------------------------------------
 epub_show_urls = 'footnote'
